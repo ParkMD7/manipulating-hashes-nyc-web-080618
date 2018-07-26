5978ec920f772e9dd6108a66c1737f86612c01d6
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-26 03:51:29 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,9 +18,10 @@ def first_challenge
   contacts.each do |person, data|
      data.each do |attribute, value|
        if attribute == :favorite_icecream_flavors
-         v.delete_if {|x|x=="strawberry"}
+         v.delete_if {|flavor| flavor == "strawberry"}
         end
       end
+    end
 
   #remember to return your newly altered contacts hash!
   contacts
